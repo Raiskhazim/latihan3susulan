b77a5b0c267f6b07a98557a8d3e42d1ceb1bc8bf
Raiskhazim <raiskhazim05@gmail.com>
2025-06-24 16:44:32 +0800

    initial commit

---
+++ BangunDatar.java
@@ -0,0 +1,13 @@
+package latrihan3susulan;
+
+/**
+ * Nama : [Isikan Nama Anda]
+ * NPM : [ Isikan NPM Anda]
+ * kelas: [Isikan Kelas Anda]
+ */
+public class BangunDatar {
+    // Contoh properti atau metode 
+    public void info() {
+        system.ot.println
+    }
+}
