11c537b0259dd2530735677de3184ed820407adb
Raiskhazim <raiskhazim05@gmail.com>
2025-06-24 16:57:32 +0800

    membuat accessor dan construktor

--- BangunDatar.java
+++ BangunDatar.java
@@ -1,13 +1,41 @@
-package latrihan3susulan;
+package latihan3susulan;
 
 /**
  * Nama : [Isikan Nama Anda]
- * NPM : [ Isikan NPM Anda]
- * kelas: [Isikan Kelas Anda]
+ * NPM  : [Isikan NPM Anda]
+ * Kelas: [Isikan Kelas Anda]
  */
 public class BangunDatar {
-    // Contoh properti atau metode 
+    // Atribut bertipe desimal
+    public double alas;
+    public double tinggi;
+    public double jariJari;
+    public double hasil;
+
+    public BangunDatar() {
+        alas = 0.0;
+        tinggi = 0.0;
+        jariJari = 0.0;
+        hasil = 0.0;
+    }
+
+    public void hitungLuasSegitiga() {
+        hasil = 0.5 * alas * tinggi;
+    }
+
+    // Method untuk menghitung luas lingkaran
+    public void hitungLuasLingkaran() {
+        hasil = 3.14 * jariJari * jariJari;
+    }
+
+    public double gethasil() {
+        return hasil;
+    }
     public void info() {
-        system.ot.println
+        System.out.println("Hasil perhitungan: " + hasil);
+    }
+
+    public void info() {
+        System.out.println("Class BangunDatar dengan atribut: alas, tinggi, jariJari, hasil");
     }
 }
